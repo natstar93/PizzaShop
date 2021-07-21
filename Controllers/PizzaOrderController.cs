376853406344bb1e06064fea280e8b721b4a26d5
Natalie Akam <natalie.akam@thetrainline.com>
2021-07-21 17:10:06 +0100

    Add PizzaOrder class and Controller

---
+++ Controllers/PizzaOrderController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace PizzaShop.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PizzaOrderController : ControllerBase
+    {
+        private readonly ILogger<PizzaOrderController> _logger;
+
+        public PizzaOrderController(ILogger<PizzaOrderController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet] // [HttpGet("pizzaorders")] doesn't work btw
+        public IEnumerable<PizzaOrder> Get()
+        {
+
+            PizzaOrder pizzaOrder = new PizzaOrder{
+                OrderId = 1,
+                PizzaName = "Buffalo",
+            };
+
+            PizzaOrder pizzaOrder2 = new PizzaOrder{
+                OrderId = 2,
+                PizzaName = "Vegano",
+            };
+
+            return new[] {pizzaOrder, pizzaOrder2};
+        }
+    }
+}
---
+++ PizzaOrder.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace PizzaShop
+{
+    public class PizzaOrder
+    {
+        public int OrderId { get; set; }
+
+        public string PizzaName { get; set; }
+
+    }
+}
